e84e589adb58259335f2a557f188363f96fc9c4d
vinod <vinod890@gmail.com>
2024-09-16 21:32:42 +0530

    1609

---
+++ Arrays/Any_table.java
@@ -0,0 +1,14 @@
+package Arrays;
+import java.util.*;
+
+public class Any_table {
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter any number:");
+    int n=sc.nextInt();
+    for(int i=1;i<=10;i++){
+        System.out.println(n +" *"+i+"="+(n*i));
+    }
+    }
+    
+}
