aee47b7fc594b4e620a75a98ca40cbf522a1f392
vinod <vinod890@gmail.com>
2024-09-20 08:24:02 +0530

    2009

--- Arrays/Max_max.java
+++ Arrays/Max_max.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Max_max {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-           int arr[]=new int [5];
+           int arr[]=new int [5]; 
            System.out.println("Enter element:");
            for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
@@ -13,7 +13,7 @@ public class Max_max {
            int max =arr[0];
            for(int i=0;i<arr.length;i++){
             if(arr[i]>max){
-                min=arr[i];
+                max=arr[i];
             }
             if(arr[i]<min){
                 min=arr[i];
---
+++ Pattern/First.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class First {
+
+    public static void main(String[] args) {
+        for(int row=1;row<=5;row++){
+            for(int j=1;j<=row;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
