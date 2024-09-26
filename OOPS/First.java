457f9487dcc3999aac0657dc341db2649fa89eb3
vinod <vinod890@gmail.com>
2024-09-26 14:13:06 +0530

    2609

--- OOPS/First.java
+++ OOPS/First.java
@@ -3,10 +3,25 @@ package OOPS;
 public class First {
     public static void main(String[] args) {
         Student s1=new Student("Vinod", 20);
-        System.out.println(s1.name);
+        // System.out.println(s1.name);
+        BankAccount BA=new BankAccount();
+        BA.username="Vinod23";
+        BA.Setpassword("Vinod@123");
+        System.out.println(BA.username);
+
         
     }
     
+}
+
+
+class BankAccount{
+    public String username;
+     private String Password;
+     public void Setpassword(String Pwd){
+        Password=Pwd;
+        
+     }
 }
 class Student{
      static String College="GLA";//static variable
